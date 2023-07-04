f53b7b272211b89c3939e954e5a389d4d71620e1
SilviaGuerra <silvia.guerrahdz@gmail.com>
2023-07-04 14:22:13 -0600

    Add packages react navigation, base

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,14 +1,19 @@
-import React, {useState, useEffect} from 'react';
-import {
-  ActivityIndicator,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
+import React from 'react';
+import {ActivityIndicator} from 'react-native';
 import {useQuery} from '@tanstack/react-query';
 import {useNavigation} from '@react-navigation/native';
+import {
+  Box,
+  Heading,
+  Image,
+  Text,
+  HStack,
+  Pressable,
+  Center,
+  AspectRatio,
+} from 'native-base';
 import {fetchFn} from '../utils/api';
+import {formatNumber, getTypeColor} from '../utils/helpers';
 
 interface PokeLinkProps {
   url: string;
@@ -25,19 +30,31 @@ interface PokeCardProps {
       };
     };
   };
+  types: {
+    slot: number;
+    type: {
+      name: string;
+    };
+  }[];
 }
 
 const PokeCard = ({url, name}: PokeLinkProps) => {
+  const navigation = useNavigation();
   const {isLoading, error, data} = useQuery<PokeCardProps>(
     ['pokemon', name],
     () => fetchFn(url),
   );
 
   return (
-    <TouchableOpacity
-      style={styles.container}
-      // onPress={() => navigation.navigate('Details', {name})}
-    >
+    <Pressable
+      flex={1}
+      m="1.5"
+      p="4"
+      backgroundColor={
+        data ? getTypeColor(data.types[0].type.name) + '.500' : ''
+      }
+      borderRadius={10}
+      onPress={() => navigation.navigate('Details', {name})}>
       {(() => {
         if (isLoading) {
           return <ActivityIndicator />;
@@ -45,30 +62,41 @@ const PokeCard = ({url, name}: PokeLinkProps) => {
       })()}
       {!data || error ? null : (
         <>
-          <Image
-            source={{
-              uri: data.sprites.other['official-artwork'].front_default,
-            }}
-            style={styles.image}
-          />
-          <Text>{data.name}</Text>
+          <Center>
+            <AspectRatio ratio={1} width="80%">
+              <Image
+                source={{
+                  uri: data.sprites.other['official-artwork'].front_default,
+                }}
+                alt="Pokeimage"
+              />
+            </AspectRatio>
+          </Center>
+          <HStack justifyContent="space-between" mb={2}>
+            <Heading textTransform="capitalize" color="white">
+              {data.name}
+            </Heading>
+            <Text color="white">#{formatNumber(data.id)}</Text>
+          </HStack>
+          <HStack>
+            {data.types.map(type => (
+              <Box
+                key={type.type.name}
+                px="2"
+                mr="1"
+                backgroundColor={getTypeColor(type.type.name) + '.400'}
+                borderRadius={10}
+                _text={{
+                  color: 'white',
+                }}>
+                {type.type.name}
+              </Box>
+            ))}
+          </HStack>
         </>
       )}
-    </TouchableOpacity>
+    </Pressable>
   );
 };
 
 export default PokeCard;
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  image: {
-    width: 100,
-    height: 100,
-    marginRight: 32,
-  },
-});
